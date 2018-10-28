db54cdf54d2f0c6ea17626e3edef51e5eb0f6f40
jan-ernst <jn.ernst1@gmail.com>
2018-10-28 19:20:17 +0100

    Backend extended with query values to only show specific slices of posts

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -67,14 +67,24 @@ router.put(
   });
 });
 
+
 router.get("", (req, res, next) => {
-  Post.find().then(documents => {
+  const pageSize = +req.query.pageSize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  if (pageSize && currentPage) {
+    postQuery
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+  postQuery.find().then(documents => {
     res.status(200).json({
       message: "Posts fetched successfully!",
       posts: documents
     });
   });
 });
+
 
 router.get("/:id", (req, res, next) => {
   Post.findById(req.params.id).then(post => {
